9928e1592b7f7a5e03db2e5326f079282fd63a20
zh <zh@zhdeMacBook-Pro.local>
2022-11-16 17:30:44 +0800

    冒泡

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module zh
 
 go 1.16
 
+require golang.org/x/net v0.1.0 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,58 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := []int{2, 8, 4, 2, 0, 1, 5, 8, 3}
+	fmt.Println(bubble(arr))
 }
 
+//冒泡 两两交换
+func bubble(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				temp := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+			}
+		}
+	}
+
+	return arr
+}
+
+// 最长回文子串 babad 正着读和反着读都是一样的。
+func longestPalindrome() string {
+	return ""
+}
+
+/* 深度优先、广度优先 */
+
 type treeNode struct {
-	Data int
-	Left *treeNode
+	Data  int
+	Left  *treeNode
 	Right *treeNode
 }
 
-/*
+/*子树
+深度优先搜索？
 */
-func isTreeSubstructure(root *treeNode, sub *treeNode) bool{
+func isTreeSubstructure(root *treeNode, sub *treeNode) bool {
 	return false
 }
 
+/*x:=[]int{4:44,55,66,1:77,88}
+fmt.Println(len(x),x[2])
+a,b:=f([]string{"test"})
+fmt.Println(a==nil,b==nil)*/
+func f(x []string) (_, _ []string) {
+	_, _ = x, x
+	return
+}
+
 // 1. golang强类型type自定义类型和原生的是等价的，但是用相同原生类型定义出的两个自定义类型，他们之间是不相等的。
-func typeConversion(){
+func typeConversion() {
 	/*type test []string
 	var t test
 	var s []string
