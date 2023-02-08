7f95d602907aeab4e42cee96ebfcd29ce5be3dad
zh <zh@zhdeMacBook-Pro.local>
2023-02-08 16:31:13 +0800

    非重复最长字符串

--- algorithm.go
+++ algorithm.go
@@ -7,3 +7,31 @@ package main
 4 递归
 5 双指针
 */
+
+func dupstring(s string) string {
+	var start, end int
+	var max int
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			var dup bool
+			for k := i; k < j; k++ {
+				if s[j] == s[k] {
+					dup = true
+					break
+				}
+			}
+			if dup {
+				break
+			}
+
+			if max < (j-i+1) {
+				start = i
+				end = j
+				max = j - i + 1
+			}
+
+		}
+	}
+
+	return s[start : end+1]
+}
